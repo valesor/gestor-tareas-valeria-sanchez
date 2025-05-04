cd751c7de1fac4357248fa3211b7fe31687e0592
Valeria Sánchez <90358728+valesor@users.noreply.github.com>
2025-05-04 17:26:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("./service-worker.js")
+       .register("service-worker.js");
         .then((reg) => console.log("✅ SW registrado en scope:", reg.scope))
         .catch((err) => console.error("❌ Error al registrar SW:", err));
     });
